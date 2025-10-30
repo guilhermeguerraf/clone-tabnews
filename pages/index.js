eb369910a733fe69b8f9cda59a98e8e7929c07f2
Guilherme Guerra <64871389+guilhermeguerraf@users.noreply.github.com>
2025-10-30 01:07:42 +0000

    add initial page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <div>
+        <h1>Tonta!</h1>
+        <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZmlpZGV5aTZ6MGJ0d3EwcHo5NzdkNmFtdXdlN2dneHM4empjdG5idyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Zh0ukrpK77Tos/giphy.gif"></img>
+    </div>
+}
+
+export default Home;
