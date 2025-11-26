e6fe31e1e7472638190d284e7eef5002d0fd913a
Guilherme Guerra <64871389+guilhermeguerraf@users.noreply.github.com>
2025-11-26 03:43:45 +0000

    add `lint:check` and `link:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
-    return <div>
-        <h1>De novo, você é uma trouxa!</h1>
-        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExenAxaHV3MGVoOWJ5OWd5cWN3NXNwdTBkeXlhMnBiM3I3MGlzYXNlYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iDmqlzQ6G5EqTEbnwF/giphy.gif"></img>
+  return (
+    <div>
+      <h1>De novo, você é uma trouxa!</h1>
+      <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExenAxaHV3MGVoOWJ5OWd5cWN3NXNwdTBkeXlhMnBiM3I3MGlzYXNlYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iDmqlzQ6G5EqTEbnwF/giphy.gif"></img>
     </div>
+  );
 }
 
 export default Home;
