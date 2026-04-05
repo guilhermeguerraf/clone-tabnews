a87960608e29c47df72dafd60ef01d71ca510092
Guilherme Guerra <64871389+guilhermeguerraf@users.noreply.github.com>
2026-04-05 01:09:10 +0000

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,3 +3,20 @@ test("GET to /api/v1/status should return 200", async () => {
 
   expect(response.status).toBe(200);
 });
+
+test("GET to /api/v1/status should return the datetime of the last update", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  const parsedDate = new Date(responseBody.updated_at);
+
+  expect(parsedDate.toISOString()).toEqual(responseBody.updated_at);
+});
+
+test("GET to /api/v1/status should return the status of the database", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
