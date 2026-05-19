cae1280c7bd2d490958a6a1cc01f9057e711e8c1
Guilherme Guerra <64871389+guilhermeguerraf@users.noreply.github.com>
2026-05-19 02:30:45 +0000

    refactor: use `orchestrator.clearDatabase()` in tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,60 +1,40 @@
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
 });
 
-beforeEach(resetDatabase);
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "post",
+          },
+        );
+        const responseBody = await response.json();
 
-async function resetDatabase() {
-  await database.query("drop schema public cascade; create schema public;");
-}
+        expect(response.status).toBe(201);
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
 
-test("POST to /api/v1/migrations should return 200 if no migrations have been performed", async () => {
-  await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "post",
+      test("For the second time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "post",
+          },
+        );
+        const responseBody = await response.json();
+
+        expect(response.status).toBe(200);
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+      });
+    });
   });
-
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "post",
-  });
-
-  expect(response.status).toBe(200);
-});
-
-test("POST to /api/v1/migrations should return 201 if at least one migration has been performed", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "post",
-  });
-
-  expect(response.status).toBe(201);
-});
-
-test("POST to /api/v1/migrations should return an array", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "post",
-  });
-
-  const responseBody = await response.json();
-
-  expect(Array.isArray(responseBody)).toBe(true);
-});
-
-test("POST to /api/v1/migrations should perform the pending migrations", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "post",
-  });
-
-  const responseBody1 = await response1.json();
-
-  expect(responseBody1.length).toBeGreaterThan(0);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "post",
-  });
-
-  const responseBody2 = await response2.json();
-
-  expect(responseBody2.length).toBe(0);
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,25 +4,18 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
+      const parsedDate = new Date(responseBody.updated_at);
 
-  expect(response.status).toBe(200);
-});
-
-test("GET to /api/v1/status should return the datetime of the last update", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-  const parsedDate = new Date(responseBody.updated_at);
-
-  expect(parsedDate.toISOString()).toEqual(responseBody.updated_at);
-});
-
-test("GET to /api/v1/status should return the status of the database", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(response.status).toBe(200);
+      expect(parsedDate.toISOString()).toEqual(responseBody.updated_at);
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
